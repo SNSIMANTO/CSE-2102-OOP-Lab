b711e8901d4ebc7922c70d80335fa9cece9593f1
SHANUR MURSALIN SIMANTO <164055165+SNSIMANTO@users.noreply.github.com>
2025-04-28 17:01:47 +0900

    Add files via upload

---
+++ 4-28-2025/DiamondProblem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:void
+
+};
+class B:public A{
+
+};
+class C:public A{
+
+};
+class D:public B:public C{
+
+};
+
+int main(){
+return 0;
+}
