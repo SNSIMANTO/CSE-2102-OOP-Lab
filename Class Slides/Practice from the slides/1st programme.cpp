f511910ac70a56cdc942a2bc1a80dca76e2b7947
SHANUR MURSALIN SIMANTO <snsimanto.cse.sub@gmail.com>
2025-05-01 12:30:53 +0600

    Practice

---
+++ Class Slides/Practice from the slides/1st programme.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class University{
+public:
+    void universityName()
+    {
+        cout<<"State University Of Bangladesh"<<endl;
+
+    }
+};
+
+int main()
+{
+    University obj;
+    obj.universityName();
+}
