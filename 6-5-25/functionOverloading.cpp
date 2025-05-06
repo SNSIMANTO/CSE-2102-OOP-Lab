5fb64f7d4dcf55139fcbd5542b3a2e269f25852a
SHANUR MURSALIN SIMANTO <snsimanto.cse.sub@gmail.com>
2025-05-06 10:01:07 +0600

    class

---
+++ 6-5-25/functionOverloading.cpp
@@ -0,0 +1,30 @@
+// Polymorphism(many forms)
+// 1.abc+syz=abcxyz (concatenation)  2. 10+20=30 (addition)
+// there are two types of polymorphism i)Static/compile ii)Dynamic/Run time
+//function: return type,function name(){}
+#include<iostream>
+using namespace std;
+class University
+{
+public:
+    void func(int a)
+    {
+        cout<<"Value of a is: "<<a<<endl;
+    }
+    void func(double a)
+    {
+        cout<<"Value of a is: "<<a<<endl;
+    }
+    void func(int a, int b)
+    {
+        cout<<"Value of a is: "<<a<<endl;
+        cout<<"Value of b is: "<<b<<endl;
+    }
+};
+int main()
+{
+    University obj;
+    obj.func(2);
+    obj.func(2);
+    obj.func(21,7);
+}
