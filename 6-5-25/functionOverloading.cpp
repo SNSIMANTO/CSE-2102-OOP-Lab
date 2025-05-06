2e70580bf6dcb7e3268d8e9a00c260fb3ebd0e0b
SHANUR MURSALIN SIMANTO <snsimanto.cse.sub@gmail.com>
2025-05-06 10:18:48 +0600

    class

--- 6-5-25/functionOverloading.cpp
+++ 6-5-25/functionOverloading.cpp
@@ -2,6 +2,7 @@
 // 1.abc+syz=abcxyz (concatenation)  2. 10+20=30 (addition)
 // there are two types of polymorphism i)Static/compile ii)Dynamic/Run time
 //function: return type,function name(){}
+// while compiling a code compiler will chick i.number of paramiter ii.Data type of the paramiter
 #include<iostream>
 using namespace std;
 class University
@@ -26,5 +27,6 @@ int main()
     University obj;
     obj.func(2);
     obj.func(2);
-    obj.func(21,7);
+    obj.func(3,8);
+    obj.func(21,7.33);//type custing
 }
